422c36cccdcd29a6658fb423b4fc582c14e788b3
Pedro Moreno <nennogabriel@gmail.com>
2020-12-21 16:50:34 -0300

    first test

---
+++ components/map.js
@@ -0,0 +1,26 @@
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const Map = (props) => {
+  return (
+    <div {...props}>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={15}
+        scrollWheelZoom={false}
+        className="h-screen w-screen -m-12"
+      >
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/nennogabriel/ckixugzdw1yx819odgzvhmhd7/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibmVubm9nYWJyaWVsIiwiYSI6ImNrZzZ0MzFmczAwbXIycm81ZDQyYzJvbTYifQ.hcHlx_YQxYmmYLV3rjNWnA`}
+        />
+        {/* <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker> */}
+      </MapContainer>
+    </div>
+  );
+};
+
+export default Map;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,33 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import dynamic from 'next/dynamic';
 
 export default function Home() {
-  return <div className={styles.container}>inicio</div>;
+  const Map = dynamic(
+    () => import('../components/map'), // replace '@components/map' with your component's location
+    {
+      loading: () => <p>A map is loading</p>,
+      ssr: false, // This line is important. It's what prevents server-side render
+    }
+  );
+  return (
+    <div className="flex justify-center items-center h-screen p-8">
+      <div className="border-8 border-black w-full h-full p-2">
+        <div className="border-8 border-black w-full h-full p-8">
+          <div className="w-full h-full overflow-hidden relative">
+            <Map className="absolute inset-0 z-0 " />
+            <div className="absolute inset-x-0 bottom-0 z-10 flex flex-col text-center">
+              <div className="bg-gradient-to-t from-white to-transparent text-3xl p-4">
+                top message
+              </div>
+              <div className="bg-white flex-auto text-green text-6xl p-4">
+                mid message
+              </div>
+              <div className="bg-white text-2xl p-4">bottom message</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="absolute inset-y-0 left-0 z-20 flex flex-col bg-blue-200">
+      </div>
+    </div>
+  );
 }
