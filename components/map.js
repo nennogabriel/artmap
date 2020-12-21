54b84f9e6e3745a9fc6d6ab7faac47b976c2ac26
Pedro Moreno <nennogabriel@gmail.com>
2020-12-21 19:38:23 -0300

    with icon

--- components/map.js
+++ components/map.js
@@ -1,11 +1,13 @@
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import mapIcon from '../utils/mapIcon';
 
 const Map = ({ apiKey, ...rest }) => {
+  const position = [-20.659998, -40.5047085];
   return (
     <div {...rest}>
       <MapContainer
-        center={[37.5650172, 126.8494659]}
+        center={position}
         zoom={13}
         scrollWheelZoom={true}
         className="h-screen w-screen -m-12"
@@ -13,11 +15,7 @@ const Map = ({ apiKey, ...rest }) => {
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/nennogabriel/ckixugzdw1yx819odgzvhmhd7/tiles/256/{z}/{x}/{y}@2x?access_token=${apiKey}`}
         />
-        {/* <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker> */}
+        <Marker position={position} icon={mapIcon}></Marker>
       </MapContainer>
     </div>
   );
